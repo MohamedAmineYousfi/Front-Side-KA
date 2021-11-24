e6668911109682f88db24f11d3e4dc9a59253644
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-24 21:07:31 +0100

    add pic in home page

--- front-react/src/Pages/Home.js
+++ front-react/src/Pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import picture from '../assates/casque-audio-anti-bruit-musique.jpg'
 const Home = () => {
   return (    
   <div style={{width:150,fontSize:150,alignContent:'center',marginTop:150}}>
+      <img src={picture} style={{marginLeft:600,width:1250,height:500}} alt="BaffleKA"/>
       <h1>Welcome to our website!</h1>
       </div>
   );
