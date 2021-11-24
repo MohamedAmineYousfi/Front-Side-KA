c92b451a5cd4234a2ec8a7a8cfc7a626329171e1
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-24 18:30:51 +0100

    create navbar && footer

---
+++ front-react/src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const Footer = () => {
+    return (
+        <div style={{
+            backgroundColor:"black",
+    marginTop:1100,
+height:250,
+fontSize:50,
+textDecorationColor:"white",
+color:"white"}}>Footer</div>
+    );
+};
+export default Footer;
---
+++ front-react/src/NavBar/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    Nav,
+    NavLogo,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from "./NavbarElements";
+import KALOGO from '../assates/KALOGO.jpg'
+
+console.log(KALOGO)
+const Navbar = () => {
+    return (
+        <>
+           <Nav>
+            <NavLogo to="/">
+             <img src={KALOGO} style={{width:150,height:150}}/>
+            </NavLogo>
+            <Bars />
+
+            <NavMenu>
+                <NavLink to="/" activeStyle>
+                    Home
+                </NavLink>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to="/contact" activeStyle>
+                    Contact
+                </NavLink>
+                <NavLink to="/signin" activeStyle>
+                    Sign In
+                </NavLink>
+                <NavBtn>
+                    <NavBtnLink to="/sign-up">Sign Up</NavBtnLink>                
+                </NavBtn>
+            </NavMenu> 
+           </Nav> 
+        </>
+    );
+};
+export default Navbar;
