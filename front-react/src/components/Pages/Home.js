81777a0170983ab70c9135090a27e4fd2641383e
firas-debich <debichf02@gmail.com>
2021-12-20 14:07:02 +0100

    adding the image of the product that has discount

--- front-react/src/components/Pages/Home.js
+++ front-react/src/components/Pages/Home.js
@@ -3,6 +3,7 @@ import NavBar from "../NavBar/navBar";
 import Caroussel from "../Caroussel/Caroussel";
 import Category from "../Category/Category";
 import SideBar from "../SideBar/sideBar";
+import image from "../../assates/103102735_298006384923121_4821662942372246899_n.jpg"
 function Home() {
   return (
     <>
@@ -12,7 +13,9 @@ function Home() {
       <SideBar/>
      <div className="discount">
       <div className="smallContainer">1</div>
-      <div className="smallContainer">2</div>
+      <div className="smallContainer ">
+        <img src={image} className="fit-container" alt="img"/>
+      </div>
      </div>
     </>
   );
