1cea636c7813c69cbba189dcef0a5b9386221850
firas-debich <debichf02@gmail.com>
2021-12-20 20:15:22 +0100

    first container done for the slider

--- front-react/src/components/Pages/Home.js
+++ front-react/src/components/Pages/Home.js
@@ -13,8 +13,21 @@ function Home() {
       <SideBar />
       <Discount/>
       <div className="featuredItems">
-
-
+        <div className="container-featuredItems  first-container">
+          <div className="Items-style">
+          Featured Items
+          </div>
+          <div className="container-arrow">   
+            <span className="container-arrow-style">
+              <i className="fas fa-chevron-left"></i>
+            </span>
+            <span className="container-arrow-style">
+              <i className="fas fa-chevron-right"></i>
+            </span>
+          </div>
+        
+        </div>
+        <div className=" container-featuredItems second-container">2</div>
       </div>
     </>
   );
