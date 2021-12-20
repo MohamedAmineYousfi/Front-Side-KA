26089b05e122dca2f2a5a574cf743c98a702f2bc
firas-debich <debichf02@gmail.com>
2021-12-20 22:52:15 +0100

    first container footer done

--- front-react/src/components/ReusableComponent/Footer/Footer.js
+++ front-react/src/components/ReusableComponent/Footer/Footer.js
@@ -4,7 +4,35 @@ function Footer() {
     return (
       <>
       <div className='footer'>
-        <div className='footer-container first-footer-container'>1</div>
+        <div className='footer-container first-footer-container'>
+
+          <div className='first-footer-container-small'>
+          <i class="fas fa-check icon-color"></i>
+          <div>Duties and Taxes Guaranteed</div>
+          </div>
+
+  <div className='first-footer-container-small'>
+  <i class="fas fa-check icon-color"></i>
+          <div>Free Express Shipping</div>
+          </div>
+          <div className='first-footer-container-small'>
+          <i class="fas fa-check icon-color"></i>
+          <div>Customer Love</div>
+          </div>
+          <div className='first-footer-container-small'>
+          <i class="fas fa-check icon-color"></i>
+          <div>Easy Returns</div>
+          </div>
+          <div className='first-footer-container-small'>
+          <i class="fas fa-check icon-color"></i>
+          <div>Secure Payment</div>
+          </div>
+
+           </div>
+      
+         
+
+      
         <div className='footer-container second-footer-container'>2</div>
         <div className='footer-container third-footer-container'> 
          <div className='third-footer-container-text'>&copy; 2019. Crisp theme Developed by Belvg</div>   
