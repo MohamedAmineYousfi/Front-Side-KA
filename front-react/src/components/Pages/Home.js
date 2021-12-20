46e77fb82bf6b0c2af849a76f4341a9c1bdc5657
firas-debich <debichf02@gmail.com>
2021-12-20 20:57:54 +0100

    most poplar and featured component done

--- front-react/src/components/Pages/Home.js
+++ front-react/src/components/Pages/Home.js
@@ -4,6 +4,8 @@ import Caroussel from "../Caroussel/Caroussel";
 import Category from "../Category/Category";
 import SideBar from "../SideBar/sideBar";
 import Discount from "../Discount/Discount";
+import FeatruedItems from "../featuredItems/FeatruedItems";
+import MostPopular from "../featuredItems/MostPopular";
 function Home() {
   return (
     <>
@@ -11,24 +13,9 @@ function Home() {
       <Caroussel />
       <Category />
       <SideBar />
-      <Discount/>
-      <div className="featuredItems">
-        <div className="container-featuredItems  first-container">
-          <div className="Items-style">
-          Featured Items
-          </div>
-          <div className="container-arrow">   
-            <span className="container-arrow-style">
-              <i className="fas fa-chevron-left"></i>
-            </span>
-            <span className="container-arrow-style">
-              <i className="fas fa-chevron-right"></i>
-            </span>
-          </div>
-        
-        </div>
-        <div className=" container-featuredItems second-container">2</div>
-      </div>
+      <Discount />
+     <FeatruedItems/>
+      <MostPopular/>
     </>
   );
 }
