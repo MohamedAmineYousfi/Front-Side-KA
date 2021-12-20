7acd35006c9eab2e2e1541bb511a5a3022d661b6
firas-debich <debichf02@gmail.com>
2021-12-20 22:12:08 +0100

    last container footer done

--- front-react/src/components/ReusableComponent/Footer/Footer.js
+++ front-react/src/components/ReusableComponent/Footer/Footer.js
@@ -4,7 +4,13 @@ function Footer() {
     return (
       <>
       <div className='footer'>
-sc
+        <div className='footer-container first-footer-container'>1</div>
+        <div className='footer-container second-footer-container'>2</div>
+        <div className='footer-container third-footer-container'> 
+         <div className='third-footer-container-text'>&copy; 2019. Crisp theme Developed by Belvg</div>   
+        
+        </div>
+
       </div>
       </>
     )
